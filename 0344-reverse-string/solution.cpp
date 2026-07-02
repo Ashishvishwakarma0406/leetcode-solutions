2c629f11fc2478c1701f70c947a722788a906291
Ashish Vishwakarma <158468242+Ashishvishwakarma0406@users.noreply.github.com>
2026-07-02 18:30:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 27.3 MB (15.32%)

--- 0344-reverse-string/solution.cpp
+++ 0344-reverse-string/solution.cpp
@@ -1,12 +1,14 @@
 class Solution {
+    void restring(vector<char> &s, int start, int end){
+        if(start>=end) return;
+        swap(s[start],s[end]);
+        return restring(s,start+1, end-1);
+    }
 public:
     void reverseString(vector<char>& s) {
-        int i=0;
-        int j=s.size()-1;
-        while(i<j){
-            swap(s[i],s[j]);
-            i++;
-            j--;
-        }
+        int start=0;
+        int end= s.size()-1;
+        restring(s,start,end);
+        return;
     }
 };
