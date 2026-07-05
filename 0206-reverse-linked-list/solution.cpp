f83890cea20ae532f87212e0b191f3588be25319
Ashish Vishwakarma <158468242+Ashishvishwakarma0406@users.noreply.github.com>
2026-07-05 11:52:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.2 MB (92.95%)

--- 0206-reverse-linked-list/solution.cpp
+++ 0206-reverse-linked-list/solution.cpp
@@ -10,28 +10,21 @@
  */
 class Solution {
 public:
-    void insert(vector<int> &arr,ListNode* head){
-        if(head==nullptr)return;
-        arr.push_back(head->val);
-        head=head->next;
-        return insert(arr,head);
-    }
-    void insertInLL(vector<int> &arr,ListNode* head,int n){
-        if(n<0)return;
-        head->val=arr[n];
-        head= head->next;
-        n=n-1;
-        return insertInLL(arr,head,n);
-    }
     ListNode* reverseList(ListNode* head) {
-        ListNode* temp= head;
-        vector<int> arr;
-        insert(arr,temp);
-        ListNode* temp2= head;
-        int n = arr.size()-1;
-        insertInLL(arr,temp2,n);
-
+        ListNode* curr = head;
+        if(!head || !head->next)return head;
+        ListNode* next1 = head->next;
+        ListNode* temp = head;
+        curr->next=nullptr;
+        while(next1->next!=nullptr){
+            temp = next1->next;
+            next1->next = curr;
+            curr = next1;
+            next1 = temp;
+        }
+        next1->next=curr;
+        head = next1;
         return head;
-
     }
+
 };
