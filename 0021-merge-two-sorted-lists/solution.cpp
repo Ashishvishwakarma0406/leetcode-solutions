ea6f68927a0c088aa4b24211d1e438f4fd8f3ed7
Ashish Vishwakarma <158468242+Ashishvishwakarma0406@users.noreply.github.com>
2026-07-05 04:43:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (62.55%)

--- 0021-merge-two-sorted-lists/solution.cpp
+++ 0021-merge-two-sorted-lists/solution.cpp
@@ -10,49 +10,42 @@
  */
 class Solution {
 public:
-    ListNode* solve(ListNode* list1, ListNode* list2) {
+    ListNode* solve(ListNode* list1, ListNode* list2){
         if (list1->next == nullptr) {
             list1->next = list2;
             return list1;
         }
-
-        ListNode* prev = list1;
+        ListNode* prev= list1;
         ListNode* curr = prev->next;
         ListNode* temp = list2;
-        ListNode* temp2 = nullptr;
-
-        while (curr != nullptr && temp != nullptr) {
-            if (prev->val <= temp->val && curr->val >= temp->val) {
+        ListNode* temp2= nullptr;
+        while(temp!=nullptr && curr!=nullptr){
+            if((prev->val<=temp->val) && (curr->val > temp->val)){
                 temp2 = temp->next;
-                prev->next = temp;
-                temp->next = curr;
-
+                prev->next=temp;
+                temp->next =curr;
                 prev = temp;
                 temp = temp2;
-            } else {
+            }else{
                 prev = curr;
-                curr = curr->next;
-
-                if (curr == nullptr) {
+                curr= curr->next;
+                if(curr==nullptr){
                     prev->next = temp;
                     return list1;
                 }
             }
         }
-
         return list1;
+
     }
-
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (list1 == nullptr)
-            return list2;
-
-        if (list2 == nullptr)
-            return list1;
-
-        if (list1->val <= list2->val)
-            return solve(list1, list2);
-        else
-            return solve(list2, list1);
+        if(list1==nullptr)return list2;
+        if(list2==nullptr)return list1;
+        if(list1->val<=list2->val){
+            return solve(list1,list2);
+        }else{
+            return solve(list2,list1);
+        }
+        
     }
 };
